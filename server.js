d4f70baf159c75e9478de092bc8afabc17b3be3c
Matt Helm <mthelm85@gmail.com>
2018-09-06 18:57:21 -0500

    changed route name, added cors

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
+
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  credentials: true,
+  methods: ['POST', 'GET', 'PATCH']
+}
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+app.use(cors({corsOptions}))
 
 const t3 = require('./mlp.js')
 
-app.post('/config', (req, res) => {
-  res.json({ Result: t3.t3(req.body.config) })
+app.post('/get-probability', (req, res) => {
+  res.json({ Result: t3.t3(req.body.array) })
 })
 
 app.listen(port)
